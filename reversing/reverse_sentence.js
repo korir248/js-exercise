b91beca9ccc171c3ff093504c9e7d098475e2558
Eugene <korir16071@gmail.com>
2021-10-07 09:53:21 +0300

    Added files on reverse assignment

---
+++ reversing/reverse_sentence.js
@@ -0,0 +1,9 @@
+var myString = "An example of a string";
+
+var myStringSplit =  myString.split(" ")
+
+myStringSplit = myStringSplit.reverse()
+
+var myStringReverse= myStringSplit.join(" ")
+
+console.log(myStringReverse);
